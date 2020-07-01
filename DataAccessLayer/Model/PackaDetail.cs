ac321aebc42728eb48231a438921399b32538f5e
Hassan Farooqui <hassan_farooqui90@yahoo.com>
2020-07-01 17:56:29 +0500

    Add PackageDetail Class in API

---
+++ DataAccessLayer/Model/PackaDetail.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Model
+{
+    [Table("Table_W_PackageDetail")]
+   public class PackaDetail
+    {
+        [Key]
+        [Column(Order = 0)]
+        public int PackageDetailID { get; set; }
+        public int PackageMasterId { get; set; }
+        public int TestRegID { get; set; }
+        public double TestCharges { get; set; }
+        public double DiscountPerc { get; set; }
+
+        public double DiscountAmount { get; set; }
+
+        public double NetCharges { get; set; }
+
+        public bool Isactive { get; set; }
+
+        public int CreatedBy { get; set; }
+
+        public Nullable<DateTime> CreatedOn { get; set; }
+
+        public int UpdatedBy { get; set; }
+
+        public Nullable<DateTime> UpdatedOn { get; set; }
+    }
+}
